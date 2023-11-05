f79b92993108f49cc638dc335a681822cc95df6f
kris9854 <43323270+kris9854@users.noreply.github.com>
2023-11-05 21:02:53 +0100

    update initialize

--- scripts/initialize_ansible_repo.sh
+++ scripts/initialize_ansible_repo.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-# README
+#-----------
+# SCRIPT CREATED BY KRISTIAN
+# ----------------------------
 # This script initializes a new Ansible repository with a specific directory structure.
 # The structure includes directories for roles, inventories, collections, and variables,
 # along with example requirements.yml files for roles and collections sourced from both
-# Ansible Galaxy and GitHub.
+# Ansible Galaxy and GitHub. It also creates an ansible.cfg configuration file and
+# a vault password file with a random 30-character password.
 #
 # Usage:
 #   ./initialize_ansible_repo.sh <ansible_repo_path>
@@ -13,15 +15,19 @@
 #
 
 # Ensure a directory path is provided
+# --------------------------------------
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <ansible_repo_path>"
   exit 1
 fi
 
 # Define the Ansible repository path
+# ------------------------------------
 ANSIBLE_REPO=$(realpath "$1")
+REPO_NAME=$(basename "${ANSIBLE_REPO}")
 
 # Define directories to be created
+#-----------------------------------
 DIRECTORIES=(
   "roles"
   "inventories"
@@ -30,6 +36,7 @@ DIRECTORIES=(
 )
 
 # Create the Ansible repository directory and directories
+# ----------------------------------------------------------
 for dir in "${DIRECTORIES[@]}"; do
   FULL_PATH="${ANSIBLE_REPO}/${dir}"
   mkdir -p "${FULL_PATH}"
@@ -37,6 +44,7 @@ for dir in "${DIRECTORIES[@]}"; do
 done
 
 # Create roles/requirements.yml file with examples from Ansible Galaxy and GitHub
+# ----------------------------------------------------------------------------------
 ROLES_REQ="${ANSIBLE_REPO}/roles/requirements.yml"
 cat <<EOL > "$ROLES_REQ"
 # Example role requirements from Ansible Galaxy
@@ -52,21 +60,23 @@ EOL
 echo "File created: ${ROLES_REQ}"
 
 # Create collections/requirements.yml file with examples from Ansible Galaxy and GitHub
+# ---------------------------------------------------------------------------------------
 COLLECTIONS_REQ="${ANSIBLE_REPO}/collections/requirements.yml"
 cat <<EOL > "$COLLECTIONS_REQ"
-# Example collection requirements from Ansible Galaxy
-- name: ansible.builtin
-  version: "1.0.0"
-  source: https://galaxy.ansible.com
+## Example collection requirements from Ansible Galaxy
+#- name: ansible.builtin
+#  version: "1.0.0"
+#  source: https://galaxy.ansible.com
 
-# Example collection requirements from GitHub
-- name: example_collection_github
-  src: https://github.com/user/ansible-collection-example
-  version: main
+## Example collection requirements from GitHub
+#- name: example_collection_github
+#  src: https://github.com/user/ansible-collection-example
+#  version: main
 EOL
 echo "File created: ${COLLECTIONS_REQ}"
 
 # Create README.md file
+#-----------------------
 README="${ANSIBLE_REPO}/README.md"
 cat <<EOL > "$README"
 # Ansible Repository
@@ -92,4 +102,64 @@ ansible-playbook -i inventories/hosts.ini example-playbook.yml
 EOL
 echo "File created: ${README}"
 
+# Create ansible.cfg file
+# -------------------------
+ANSIBLE_CFG="${ANSIBLE_REPO}/ansible.cfg"
+cat <<EOL > "$ANSIBLE_CFG"
+#configs
+[defaults]
+#inventory = inventories/${REPO_NAME}/inventory.ini
+roles_path = ~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:./roles
+filter_plugins=~/.ansible/plugins/filter:/usr/share/ansible/plugins/filter:~/plugins/filter
+host_key_checking = False
+vault_password_file = ~/vault_keys/${REPO_NAME}
+stdout_callback = ansible.builtin.default
+
+[privilege_escalation]
+
+[colors]
+highlight = white
+verbose = blue
+warn = bright purple
+error = red
+debug = dark gray
+deprecate = purple
+skip = cyan
+unreachable = red
+ok = green
+changed = yellow
+diff_add = green
+diff_remove = red
+diff_lines = cyan
+
+[galaxy]
+server_list = automation_hub, galaxy_opensource
+
+[galaxy_server.automation_hub]
+url=https://console.redhat.com/api/automation-hub/content/published/
+auth_url=https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token
+token=URKEYS
+[galaxy_server.galaxy_opensource]
+url=https://galaxy.ansible.com/
+EOL
+echo "File created: ${ANSIBLE_CFG}"
+
+# Create the vault password file with a random 30-character password
+# --------------------------------------------------------------------
+VAULT_DIR="${HOME}/.vault_keys"
+mkdir -p "${VAULT_DIR}"
+VAULT_PW_FILE="${VAULT_DIR}/.${REPO_NAME}"
+RANDOM_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c45; echo '')
+echo "${RANDOM_PASSWORD}" > "${VAULT_PW_FILE}"
+echo "File created: ${VAULT_PW_FILE}"
+
+if [[ $EUID -eq 0 ]]; then
+  chmod 0660 "${VAULT_DIR}"
+  chmod 0660 "${VAULT_PW_FILE}"
+fi
+
+if [[ $EUID -ne 0 ]]; then
+  echo -e "\033[1;34mThis script isn't run as sudo so we can't set the correct ownership on the directory\033[0m"
+
+fi
 echo "Ansible repository layout created at ${ANSIBLE_REPO}"
