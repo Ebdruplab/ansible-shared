eb9c15b1a4239d09fe69ac7a530a1bead52282be
Kristian <43323270+kris9854@users.noreply.github.com>
2024-01-23 15:14:31 +0100

    Update initialize_ansible_repo.sh
    Added a better init_script.sh

--- scripts/initialize_ansible_repo.sh
+++ scripts/initialize_ansible_repo.sh
@@ -168,15 +168,30 @@ echo "File created: ${GITIGNORE_FILE}"
 INIT_SCRIPT="${ANSIBLE_REPO}/scripts/init_script.sh"
 cat <<EOL > "$INIT_SCRIPT"
 #!/bin/bash
-# This script installs requirements from requirements.yml files in roles and collections directories
+
+# Check if ANSIBLE_REPO is set
+if [ -z "${ANSIBLE_REPO}" ]; then
+    # ANSIBLE_REPO is not set, use the current directory
+    ANSIBLE_REPO=$(pwd)
+
+    # Ask the user for confirmation
+    read -p "ANISBLE_REPO is not set. Do you want to use the current directory ${ANSIBLE_REPO}? (y/n) " -n 1 -r
+    echo # Move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        # If the user does not confirm, exit the script
+        echo "Aborted."
+        exit 1
+    fi
+fi
 
 echo "Installing requirements for roles..."
-ansible-galaxy role install -r "\${ANSIBLE_REPO}/roles/requirements.yml" -p "\${ANSIBLE_REPO}/roles"
+ansible-galaxy role install -r "${ANSIBLE_REPO}/roles/requirements.yml" -p "${ANSIBLE_REPO}/roles"
 
 echo "Installing requirements for collections..."
-ansible-galaxy collection install -r "\${ANSIBLE_REPO}/collections/requirements.yml" -p "\${ANSIBLE_REPO}/roles"
+ansible-galaxy collection install -r "${ANSIBLE_REPO}/collections/requirements.yml" -p "${ANSIBLE_REPO}/collections"
 
 echo "Requirements installation complete."
+
 EOL
 
 echo "Init script created: ${INIT_SCRIPT}"
